faaabb11789ee6462eed809be5d1a70dbb97ab66
Alfred Fantone <alfredrick.fantone@edu.sait.ca>
2026-03-26 09:34:07 -0600

    Implement Javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Alfred Fantone
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -6,67 +6,88 @@ You are encouraged to use the provided naming convention for ease of review.
 
 */
 
-/****************** create variables ******************/
-/* create variables to hold the values for modelName and duration */
+// Use a DOMContentLoaded listener since script.js is declared without a "defer" attribute.
+document.addEventListener("DOMContentLoaded", function onDOMContentLoaded(event) {
+    // Since script.js is declared twice, there's going to be two 
+    // listeners for this event, we only want one to run.
+    event.stopImmediatePropagation();
 
-// INSERT YOUR CODE HERE
+    /****************** create variables ******************/
+    /* create variables to hold the values for modelName and duration */
 
+    // INSERT YOUR CODE HERE
+    // Use the default value declared in index.html
+    let modelName = "XYZ";
+    let duration = 0;
 
+    /****************** helper function ******************/
+    /* create a function called recalculate() which will
+        - create a variable to represent the calculated-cost span element. That will look something like:
+            // let costLabel = document.getElementById("calculated-cost");
+        - check the value of the modelName variable, and use that to calculate the new total cost:
+            e.g. if modelName is currently "XYZ", duration * 100 gives us the new total cost.
+            if modelName is currently "CPRG", duration * 213 gives us the new total cost.
+        - set the value of the calculated-cost element's innerHTML to this new value
+    */
 
+    // INSERT YOUR CODE HERE
+    function recalculate() {
+        const costLabel = document.getElementById("calculated-cost");
+        // Since modelName will only have two values, we can just use a tenary operator here...
+        const newTotalCost = modelName === "XYZ" ? duration * 100 : duration * 123;
+        // Use Number.toFixed(2) to get the same exact amount of decimal places as the placeholder.
+        costLabel.textContent = newTotalCost.toFixed(2);
+    }
 
+    /****************** model button logic ******************/
 
+    /* 
+    - first, create a variable to represent the "Switch Model" pseudo-button (hint: can use getElementById)
+    - second, create a function called changeModel() which checks the value of the model name variable. This function will:
+        - create a variable to represent the model-text span element
+        - if modelName is currently "XYZ", change the value of modelName to "CPRG", and change the innerHTML of the model-text span element to "Model CPRG"
+        - if modelName is currently "CPRG", change the value of modelName to "XYZ", and change the innerHTML of the model-text span element to "Model XYZ"
+        - then, recalculate() the total cost.
+    - finally, uncomment the following line of JavaScript to have this function run automatically whenever the pseudo-button is clicked: */
+        // modelButton.addEventListener("click", changeModel);
 
+    // INSERT YOUR CODE HERE
+    const modelButton = document.getElementById("model-button");
+    modelButton.addEventListener("click", changeModel);
 
-/****************** helper function ******************/
-/* create a function called recalculate() which will
-    - create a variable to represent the calculated-cost span element. That will look something like:
-        // let costLabel = document.getElementById("calculated-cost");
-    - check the value of the modelName variable, and use that to calculate the new total cost:
-        e.g. if modelName is currently "XYZ", duration * 100 gives us the new total cost.
-        if modelName is currently "CPRG", duration * 213 gives us the new total cost.
-    - set the value of the calculated-cost element's innerHTML to this new value
-*/
+    function changeModel() {
+        const modelTextSpan = document.getElementById("model-text");
+        // Toggle modelName between XYZ and CPRG.
+        modelName = modelName === "XYZ" ? "CPRG" : "XYZ";
+        modelTextSpan.textContent = "Model " + modelName;
+        recalculate();
+    }
 
-// INSERT YOUR CODE HERE
+    /****************** duration button logic ******************/
+    /*  - first, create a variable to represent the "Change Duration" pseudo-button.
+        - then, create a function called changeDuration() that will
+            - create a variable to represent the duration-text span element
+            - prompt() the user for a new duration
+            - save the result of the prompt() to the duration variable
+            - change the innerHTML of the duration-text span element to this new value
+            - recalculate() the total cost/
+        - finally, attach this function to the "Change Duration" pseudo-button, so it runs whenever the button is clicked.
+    */
 
+    // INSERT YOUR CODE HERE
+    const durationButton = document.getElementById("duration-button");
+    durationButton.addEventListener("click", changeDuration);
 
+    function changeDuration() {
+        const durationTextSpan = document.getElementById("duration-text");
+        const newDuration = Number(prompt("Please Enter a New Duration:"));
 
+        // Validate the user inputted duration...
+        if (!Number.isNaN(newDuration)) {
+            duration = newDuration;
+            durationTextSpan.textContent = newDuration.toString();
+            recalculate();
+        }
+    }
 
-
-
-
-/****************** model button logic ******************/
-
-/* 
-- first, create a variable to represent the "Switch Model" pseudo-button (hint: can use getElementById)
-- second, create a function called changeModel() which checks the value of the model name variable. This function will:
-    - create a variable to represent the model-text span element
-    - if modelName is currently "XYZ", change the value of modelName to "CPRG", and change the innerHTML of the model-text span element to "Model CPRG"
-    - if modelName is currently "CPRG", change the value of modelName to "XYZ", and change the innerHTML of the model-text span element to "Model XYZ"
-    - then, recalculate() the total cost.
-- finally, uncomment the following line of JavaScript to have this function run automatically whenever the pseudo-button is clicked: */
-    // modelButton.addEventListener("click", changeModel);
-
-// INSERT YOUR CODE HERE
-
-
-
-
-
-
-
-/****************** duration button logic ******************/
-/*  - first, create a variable to represent the "Change Duration" pseudo-button.
-    - then, create a function called changeDuration() that will
-        - create a variable to represent the duration-text span element
-        - prompt() the user for a new duration
-        - save the result of the prompt() to the duration variable
-        - change the innerHTML of the duration-text span element to this new value
-        - recalculate() the total cost/
-    - finally, attach this function to the "Change Duration" pseudo-button, so it runs whenever the button is clicked.
-*/
-
-// INSERT YOUR CODE HERE
-
-
-
+});
